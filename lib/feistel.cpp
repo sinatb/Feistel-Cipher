348d525d0ae158b59dbdc6f20d0b9a7f7c142d0b
sinatb <sinatb.dev@gmail.com>
2024-05-12 23:10:20 +0330

    feat : made the main file more understandable

--- lib/feistel.cpp
+++ lib/feistel.cpp
@@ -32,6 +32,7 @@ std::vector<uint64_t> feistel::encrypt(uint64_t lpt,uint64_t rpt)
     uint64_t r4 = calcFeistelRound(subKeys[3],r2,r3);
     return {r4,r3};
 }
+
 std::vector<uint64_t> feistel::decrypt(uint64_t lct, uint64_t rct)
 {
     uint64_t r1 = calcFeistelRound(subKeys[3],lct,rct);
@@ -40,10 +41,12 @@ std::vector<uint64_t> feistel::decrypt(uint64_t lct, uint64_t rct)
     uint64_t r4 = calcFeistelRound(subKeys[0],r2,r3);
     return {r4,r3};
 }
+
 uint64_t feistel::calcFeistelRound(key r_k, uint64_t l, uint64_t r)
 {
     uint64_t r_k_u = r_k.getKey();
     uint64_t tmp = r ^ r_k_u;
     return tmp ^ l;
 }
+
 
--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 #include "lib/key.h"
 #include "lib/feistel.h"
 using namespace std;
-int main() {
+int main()
+{
+    uint64_t p1,p2;
+    cout << "Enter the 2 numbers you want to encrypt : " << '\n';
+    cin >> p1 >> p2;
     key k;
+    cout << "The random key is : " << '\n';
     k.printKey();
-    cout <<(int)k.getWordByte(3)[0]
-         <<(int)k.getWordByte(3)[1]
-         <<(int)k.getWordByte(3)[2]
-         <<(int)k.getWordByte(3)[3]<<'\n';
+    cout << "The rotWord function result on the 3rd word is : " << '\n';
     cout << k.rotWord(3) << '\n';
-    //k.generateSubKey().printKey();
     feistel f(k);
+    cout << "The subkeys are : " << '\n';
     f.printSubKeys();
-    vector<uint64_t> enc = f.encrypt(54,32);
+    vector<uint64_t> enc = f.encrypt(p1,p2);
     vector<uint64_t> dec = f.decrypt(enc[0],enc[1]);
+    cout << "Result after encryption is :" << '\n';
     cout << enc[0] << enc[1] << '\n';
-    cout << dec[0] <<" "<< dec[1] << '\n';
+    cout << "Result after decryption is :" << '\n';
+    cout << std::dec << dec[0] <<" "<< dec[1] << '\n';
 }
